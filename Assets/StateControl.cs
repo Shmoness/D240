4d7244c9c9a25b0ae1e188748d09c8c4d96c935c
ming17 <mci10@pitt.edu>
2019-07-23 11:58:40 -0400

    Add animation transitions

---
+++ Assets/StateControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateControl : MonoBehaviour
+{
+    public Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Next()
+    {
+        m_Animator.SetBool("Next", true);
+    }
+
+    public void Prev()
+    {
+        m_Animator.SetBool("Prev", true);
+    }
+}
